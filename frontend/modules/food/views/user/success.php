c665c1be44470a6177235080cf73c3742a7e3667
root <674753084@qq.com>
2017-07-05 22:02:53 +0800

    下单成功页

--- frontend/modules/food/views/user/success.php
+++ frontend/modules/food/views/user/success.php
@@ -3,19 +3,33 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-    <title>下单成功</title>
-    <link href="/static/food/css/style.css" rel="stylesheet" type="text/css">
+    <title>容合商圈</title>
+    <link href="/static/627dc/css/style.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
 <div class="box"><!--头部：开始--->
-    <div class="confirmation">
-        <img src="/static/food/images/confirmation.png">
-        <h3>下单成功</h3>
-        <h4>请坐等服务员为您复单~</h4>
+    <div class="Favorites">
+        <ul>
+            <p>支付成功</p>
+            <li><a href="#"></a></li>
+            <div class="clear"></div>
+        </ul>
     </div>
-    <div class="examine"><a href="/food/user/my-order">查看我的订单</a></div>
-    <div class="footer"><p>©琼海玲珑软件有限公司 版权所有</p><p>联系电话：<a href="tel:089862922223">0898-62922223</a></p></div>
+    <div class="main">
+        <div class="payment_success">
+            <img src="/static/627dc/images/jfimg.png">
+            <h3>恭喜您获得<span><?=$o['total']?></span>积分</h3>
+        </div>
+        <div class="butt_box">
+            <dl class="clearfix">
+                <dt><a href="index.html">返回首页</a></dt>
+                <dd><a href="jf_record.html">积分记录</a></dd>
+            </dl>
+        </div>
+        <div class="clue_box"><span>温馨提示：</span>可兑换相应的商品可前往<a href="jf_shop.html">积分商城</a>兑换！</div>
+    </div>
+    <div class="footer"><a href="#">容合<img src="/static/627dc/images/logo.png" width="20" height="20"/>商圈</a></div>
 </div>
 </body>
 </html>
