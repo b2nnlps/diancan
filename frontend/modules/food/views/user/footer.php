71e551cd47fba1f37c332d49e4b9ebafbfdc243c
Neil <674753084@qq.com>
2017-08-05 17:09:14 +0800

    增加弹出底部

---
+++ frontend/modules/food/views/user/footer.php
@@ -0,0 +1,14 @@
+<div class="navigate">
+    <ul>
+        <a href="/food/user/shop-info">
+            <li><img src="/static/84dc/icon/syimg.png">店家</li>
+        </a>
+        <!--        <a ><li><img src="/static/84dc/icon/yyimg.png">预约</li></a>-->
+        <a href="/food/user/my-order">
+            <li><img src="/static/84dc/icon/ddimg.png">订单</li>
+        </a>
+        <a href="/food/user/person">
+            <li><img src="/static/84dc/images/nav_3.png">个人中心</li>
+        </a>
+    </ul>
+</div>
