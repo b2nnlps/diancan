c249cb7b8d012b205e140519d198318dde9a96dc
Neil <674753084@qq.com>
2017-08-06 23:28:25 +0800

    付款成功

--- frontend/modules/food/views/user/success.php
+++ frontend/modules/food/views/user/success.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-    <title>容合商圈</title>
+    <title>容合点餐</title>
     <link href="/static/627dc/css/style.css" rel="stylesheet" type="text/css">
 </head>
 
@@ -23,11 +23,11 @@
         </div>
         <div class="butt_box">
             <dl class="clearfix">
-                <dt><a href="index.html">返回首页</a></dt>
-                <dd><a href="jf_record.html">积分记录</a></dd>
+                <dt><a href="/food/user/index">返回首页</a></dt>
+                <dd><a>积分记录</a></dd>
             </dl>
         </div>
-        <div class="clue_box"><span>温馨提示：</span>可兑换相应的商品可前往<a href="jf_shop.html">积分商城</a>兑换！</div>
+        <div class="clue_box"><span>温馨提示：</span>可兑换相应的商品可前往<a>积分商城</a>兑换！</div>
     </div>
     <div class="footer"><a href="#">容合<img src="/static/627dc/images/logo.png" width="20" height="20"/>商圈</a></div>
 </div>
