11e87799dce1b7ff6ab0e4205eb58d33ca1ce05e
Neil <674753084@qq.com>
2017-08-08 12:08:36 +0800

    删除过期订单

--- frontend/modules/food/controllers/UserController.php
+++ frontend/modules/food/controllers/UserController.php
@@ -195,6 +195,8 @@ class UserController extends BaseController
 
     public function actionMyOrder()
     {
+        Order::deleteAll('user=:openid AND status=0 AND created_time<=:time', [':openid' => $this->openid, ':time' => date("Y-m-d H:i:s", time() - 86400)]);
+        //删除过期未支付订单
         $o = Order::find()->where(['user' => $this->openid])->orderBy("id desc")->all();
         return $this->render('my-order', ['o' => $o]);
     }
